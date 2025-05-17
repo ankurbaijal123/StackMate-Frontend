7c2e987a36280a3c3687d89c4418e7b0cba4f381
ankur baijal <ankur.baijal11@gmail.com>
2025-05-17 18:18:39 +0530

    add things

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <><h1>StackMate Frontend </h1>
+    </>
+  )
+}
+
+export default App
