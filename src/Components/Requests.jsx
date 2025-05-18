f1e1d0234b2a5c13c4d80494c67c31921f88060d
ankur baijal <ankur.baijal11@gmail.com>
2025-05-18 22:25:11 +0530

    feat: Connection Accept or Reject

--- src/Components/Requests.jsx
+++ src/Components/Requests.jsx
@@ -2,10 +2,27 @@ import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constansts";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 const Requests = () => {
   const dispatch = useDispatch();
+
+
+
   const requests = useSelector((store) => store.requests);
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   const fetchRequests = async () => {
     try {
       const res = await axios.get(BASE_URL + "/user/requests/received", {
@@ -22,7 +39,7 @@ const Requests = () => {
   }, []);
 
   if (!requests) return null;
-  if (requests.length === 0) return <h1>No Requests Found</h1>;
+  if (requests.length === 0) return <h1 className="flex justify-center my-10">No Requests Found</h1>;
 
   return (
     <div className="flex flex-col items-center my-10">
@@ -49,8 +66,18 @@ const Requests = () => {
               <p className="text-sm mt-1">{about}</p>
             </div>
             <div>
-              <button className="btn btn-secondary mx-3">Accept</button>
-              <button className="btn btn-primary mx-2">Reject</button>
+              <button
+                className="btn btn-secondary mx-3"
+                onClick={() => reviewRequest("accepted", request._id)}
+              >
+                Accept
+              </button>
+              <button
+                className="btn btn-primary mx-2"
+                onClick={() => reviewRequest("rejected", request._id)}
+              >
+                Reject
+              </button>
             </div>
           </div>
         );
--- src/utils/requestSlice.js
+++ src/utils/requestSlice.js
@@ -5,12 +5,13 @@ const requestSlice = createSlice({
     initialState: null,
     reducers: {
         addRequests: (state, action) => action.payload,
-        removeRequests: (state, action) => {
-           return null
+        removeRequest: (state, action) => {
+           const newArray = state.filter(req => req._id !== action.payload);
+           return newArray
         }
     }
 
 })
 
-export const {addRequests, removeRequests} = requestSlice.actions;
+export const {addRequests, removeRequest} = requestSlice.actions;
 export default requestSlice.reducer
