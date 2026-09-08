3aa21cec0f1431d90823f1687531a7810fb11784
ankurbaijal123 <ankur.baijal11@gmail.com>
2026-09-08 19:55:22 +0530

    Add: Environment configuration and socket connection updates

--- src/utils/constansts.js
+++ src/utils/constansts.js
@@ -1,2 +1,2 @@
-export const BASE_URL = location.hostname === "localhost"? "http://localhost:3000" : "/api"
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,11 +2,5 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constansts";
 
 export const createSocketConnection= () => {
-    if(location.hostname === "localhost"){
-         return io(BASE_URL)
-    }
-    else{
-        return io("/", {path: "/api/socket.io"})
-    }
-
+    return io(BASE_URL);
 }
